58769c322833e2c194b10d29125378256a9f8761
Oskipol <pilczukoskar@gmail.com>
2026-03-18 00:20:54 +0100

    TODO: nav and footer

--- app/root.tsx
+++ app/root.tsx
@@ -6,21 +6,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
+import "./styles/reset.scss";
+import "./styles/base.scss";
+import "./styles/fonts.scss";
 import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { OverviewPage } from "../components/index/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kotlin Programming Language" },
+    { name: "description", content: "Kotlin is a modern programming language." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <OverviewPage />;
 }
